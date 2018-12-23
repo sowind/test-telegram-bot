5eae0636e33cdf5441dd28c70a10c3287bac6d80
Igor <letter.igor@gmail.com>
2018-12-23 06:12:31 +0500

    More awful code.\n-Whole code refactoring.\n-New features: Get letter, Holidays presents.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const _ = require('underscore');
 const debug = require('debug')('bot');
 const SocksClient = require('socks5-https-client/lib/Agent');
 const Telegraf = require('telegraf');
 const Markup = require('telegraf/markup');
+const Session = require('telegraf/session');
+const emoji = require('node-emoji');
 
 /**
  * Подтверждение работы
@@ -34,40 +37,123 @@ const bot = new Telegraf(process.env.BOT_TOKEN,
     }
 });
 
-function botAnswer (ctx, answer, addition = null) {
-    debug(ctx.message);
-    ctx.reply(answer, addition);
-    debug('Ответ "%s" отправлен', answer);
-}
+bot.use(Session());
+
+bot.use((ctx, next) => {
+    if(ctx.message) debug(ctx.message);
+    const start = new Date();
+    return next(ctx).then((botAnswer) => {
+        if(botAnswer) {
+            const ms = new Date() - start;
+            debug('Время ответа %sms', ms);
+            //debug(botAnswer);
+            debug('Ответ "%s" отправлен.', botAnswer.text);
+        }
+    });
+});
 
 function declOfNum(number, titles) {  
     let cases = [2, 0, 1, 1, 1, 2];  
     return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];  
 }
 
-function thankYou (ctx) {
+bot.start((ctx) => {
+    return ctx.reply('Добро пожаловать!\nВыберите действие:',
+    Markup.inlineKeyboard([[
+        Markup.callbackButton('Молодец', 'good_boy'),
+        Markup.callbackButton('Запись', 'to_write')
+    ],[
+        Markup.callbackButton('Принять письмо', 'to_send'),
+    ]]).extra());
+});
+
+bot.action('good_boy', (ctx) => { 
+    return ctx.reply('Спасибо! ☺️');
+});
+
+bot.action('to_write', (ctx) => {
+    return ctx.reply('Записываю 🤔');
+});
+
+bot.action('to_letter_open', (ctx) => {
+    ctx.session.openLetter = ctx.session.openLetter || 0;
+    debug(ctx.session.openLetter);
+    return ctx.reply(!ctx.session.openLetter ? (ctx.session.openLetter++, emoji.random().emoji) : 'Уже открыто! Получите новое письмо.');
+});
+
+bot.action('to_send', (ctx) => {
+    ctx.session.openLetter = 0;
+    return ctx.reply('Вам письмо ✉️', 
+    Markup.inlineKeyboard([
+        Markup.callbackButton('Открыть', 'to_letter_open')
+    ]).extra());
+});
+
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+
+bot.hears(/^запись/i, (ctx) => ctx.reply('Записываю 🤔'));
+
+bot.hears(/^молодец(!|1|один|\))*$/i, (ctx) => {
     let ep;
     if (ctx.message && ctx.message.text.indexOf('!')) {
         ep = ctx.message.text.split('!').length - 1;
     }
     debug(ep);
-    botAnswer(ctx, 'спасибо! ☺️'+ (ep ? ` Вы похвалили на ${ep} ${declOfNum(ep, ['восклицательный', 'восклицательных', 'восклицательных'])} ${declOfNum(ep, ['знак', 'знака', 'знаков'])}!` : '')); // эпичнейший говнокод
+
+    let message = `${ep} ${declOfNum(ep, ['восклицательный', 'восклицательных', 'восклицательных'])} ${declOfNum(ep, ['знак', 'знака', 'знаков'])}`;
+    let answer = 'Спасибо! ☺️ ';
+    if (ep >= 1 && ep <= 4){
+        answer += `Вы похвалили на ${message}!`;
+    } 
+    if (ep >= 5 && ep <= 14){
+        answer += `Вы мне льстите — ${message}!`;
+    } 
+    if (ep > 15) {
+        answer += `Вас не остановить — несчетное количество восклицательных знаков.`;
+    }
+
+    return ctx.reply(answer);
+});
+
+bot.hears(/^🎅🎄(🎁)*$/, ctx => {
+    ctx.session.holidayPresents = 0;
+
+    let presents;
+    if (ctx.message.text.indexOf('🎁')) {
+        presents = ctx.session.holidayPresents = ctx.message.text.split('🎁').length - 1;
+    }
+    debug(presents);
+
+    let holidayGreeting = [`Хо-хо-хо! Дед Мороз желает тебе весёлого Нового года и Рождества, и кладет под твою ёлку ${presents} ${declOfNum(presents, ['подарок', 'подарка', 'подарков'])}! ❄️⛄️`,
+    Markup.inlineKeyboard([
+        [Markup.callbackButton('Открыть один', 'open_one_present')],
+        [Markup.callbackButton('Открыть все', 'open_all_presents')]
+    ]).extra()];
+
+    let tooMuchPresents = ['Под ёлкой не хватает места для всех подарков! Используйте меньшее количество.'];
+
+    return ctx.reply.apply(null, (presents > 9 ? tooMuchPresents : holidayGreeting));
+});
+
+function getRandomPresents(number) {
+    let presents = ['👼', '🍪', '🌟', '❄️', '☃️', '⛄️', '🎄', '🔔', '🧦'];
+    return _.sample(presents, number);
 }
 
-function botStart(ctx) {
-    let answer = ('Добро пожаловать!\nВыберите действие:');
-    let addition = Markup.inlineKeyboard([
-        Markup.callbackButton('Молодец', 'Молодец'),
-        Markup.callbackButton('Запись', 'Запись'),
-    ]).extra();
-    bot.action('Молодец', ctx => thankYou(ctx));
-    bot.action('Запись', ctx => botAnswer(ctx, 'записываю 🤔'));
-    botAnswer(ctx, answer, addition);
-}
+bot.action('open_one_present', ctx => {
+    let presents = getRandomPresents(1);
+    
+    return ctx.reply(ctx.session.holidayPresents ? (ctx.session.holidayPresents--, presents.join('')) : 'Подарки закончились.',
+    Markup.inlineKeyboard([
+        Markup.callbackButton('Открыть еще один', 'open_one_present')
+    ]).extra());
+});
 
-bot.start((ctx) => botStart(ctx));
-bot.on('sticker', (ctx) => botAnswer(ctx, '👍'));
-bot.hears(/^запись/i, (ctx) => botAnswer(ctx, 'записываю 🤔'));
-bot.hears(/^молодец(!|1|один|\))*$/i, (ctx) => thankYou(ctx))
+bot.action('open_all_presents', ctx => {
+    let presents = getRandomPresents(ctx.session.holidayPresents);
+    debug('Осталось %s', presents.length);
+    
+    return ctx.reply(ctx.session.holidayPresents ? (ctx.session.holidayPresents = 0,presents.join('')) : 'Подарки закончились.');
+});
 
 bot.startPolling();
